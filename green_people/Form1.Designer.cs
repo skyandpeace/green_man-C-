355750f0f4b816c0ca04969b8d41d9c1d33d1413
meow <johnkikijoy@gmail.com>
2019-05-24 12:50:15 +0800

    updata

--- green_people/Form1.Designer.cs
+++ green_people/Form1.Designer.cs
@@ -83,7 +83,7 @@
             this.groupBox1.Size = new System.Drawing.Size(620, 576);
             this.groupBox1.TabIndex = 254;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "小綠人";
             // 
             // frmsmallgreenman
             // 
